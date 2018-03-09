9907037308104aa4ea77e5f0d68b62ff9e74234b
Rahul Sharma <rsharma98@protonmail.com>
2018-03-09 21:37:29 +0000

    Initial Unity Files
    Created Unity files.
    Added a PlayerController script and a square as a placeholder player.
    Added functionality to make player sprite look towards mouse.

---
+++ Space Defender/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Camera cam;
+	Vector3 mousePos;
+
+	[SerializeField] float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);	//Get the mouse position in terms of the world (i.e. transform.position)
+		Vector3 pos = transform.position;	//Get the position of the ship
+	 	float angleRad = Mathf.Atan2(mousePos.y - pos.y, mousePos.x - pos.x);	//Get the angle between the ship and the mouse (in radians)
+        float angleDeg = (180 / Mathf.PI) * angleRad;	//Convert the angle to degrees
+		// Rotate Object
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angleDeg), rotSpeed * Time.deltaTime);
+	}
+}
